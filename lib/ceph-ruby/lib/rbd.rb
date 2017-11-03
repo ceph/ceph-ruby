3856e8cb38b8979cec4540fe18f28b8d5337fb1b
Marius Rieder <marius.rieder@durchmesser.ch>
2017-11-03 14:49:07 +0100

    Some basic metadata implemenation.

--- lib/ceph-ruby/lib/rbd.rb
+++ lib/ceph-ruby/lib/rbd.rb
@@ -39,6 +39,9 @@ module CephRuby
 
       attach_function 'rbd_list', [:pointer, :buffer_out, :pointer], :int
 
+      attach_function 'rbd_metadata_set', [:pointer, :string, :string], :int
+      attach_function 'rbd_metadata_list', [:pointer, :string, :int, :buffer_out, :pointer, :buffer_out, :pointer], :int
+
       class StatStruct < FFI::Struct
         layout :size, :uint64,
           :obj_size, :uint64,
